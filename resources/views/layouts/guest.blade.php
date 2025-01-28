9aa8ba042a18142d7b292436f9095c730165ea0e
munawir24 <almunawir12345@yahoo.com>
2025-01-28 15:03:46 +0700

    PENCARIAN GOOGLE

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -282,6 +282,18 @@
     {{-- <script src="{{ asset('bootstrap/dist/js/bootstrap.min.js') }}"></script> --}}
     {{-- <script src="{{ asset('bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script> --}}
     {{-- <script src="{{asset('admin/build/js/AdminLTE.js')}}"></script> --}}
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-52CT4V3GEZ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-52CT4V3GEZ');
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
