2adad72c8b5d7f4f6fb44d3a1f116fab489c1292
joynugroho <joy.justitia@gmail.com>
2025-02-20 14:59:57 +0700

    Update beranda.blade.php

--- najahhurrahman/resources/views/beranda.blade.php
+++ najahhurrahman/resources/views/beranda.blade.php
@@ -188,7 +188,7 @@
                 <div class="card" style="background: rgba(255, 255, 255, 0.768)">
                     <div class="card-body">
                         <center>
-                            <h2>Paket Haji & Umrah</h2>
+                            <h2>Paket Haji & Umroh</h2>
                         </center>
                         <div class="pt-2 row justify-content-center">
                             @foreach ($landing as $ld)
