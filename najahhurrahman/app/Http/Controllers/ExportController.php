cef53684d4e00de185eb25a251db11b89fc3d48f
munawir24 <almunawir12345@yahoo.com>
2025-03-08 14:11:40 +0700

    Update ExportController.php

--- najahhurrahman/app/Http/Controllers/ExportController.php
+++ najahhurrahman/app/Http/Controllers/ExportController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class ExportController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function formulir($id)
     {
         $no = 1;
